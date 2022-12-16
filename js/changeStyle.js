1bf8e53c5d2f253d983f77146d74e7358a359a5a
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2022-12-16 04:04:28 +0100

    Added theme change button

---
+++ js/changeStyle.js
@@ -0,0 +1,19 @@
+let styles = ["theme-alpha"]
+
+function setStyle(theme) {
+    document.getElementById("theme-css").setAttribute("href", "../css/themes/"+theme+".css");
+}
+
+function changeStyle() {
+    let currentStyle = document.getElementById("theme-css").getAttribute("href");
+    for (let i = 0; i < styles.length; i++) {
+        if (currentStyle === "../css/themes/"+styles[i]+".css" && i === styles.length-1) {
+            setStyle(styles[0]);
+            break;
+        }
+        if (currentStyle === "../css/themes/"+styles[i]+".css") {
+            setStyle(styles[i+1]);
+            break;
+        }
+    }
+}
