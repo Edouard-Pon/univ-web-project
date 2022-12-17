4c95f082579d473c4c5045cba1dd3a4b10c4b481
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2022-12-17 16:26:19 +0100

    Added style to header for phones and tablets

---
+++ js/showMenu.js
@@ -0,0 +1,5 @@
+function showMenu() {
+    let menuList = document.getElementById("navMenu");
+    if (menuList.style.display === "block") menuList.style.display = "none";
+    else menuList.style.display = "block"
+}
